b965d1aa8f67037dda93dac88e826f2534bc072c
Blazkowicz <bj.blazkowicz@gmail.com>
2020-11-09 11:35:21 +0100

    Update routes to force user to log-in

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\LibraryController::class, 'index'])->name('home');
-Route::get('/media/{library}/', [App\Http\Controllers\CollectionController::class, 'index'])->name('libraries');
-Route::get('/media/{library}/{collection}/', [App\Http\Controllers\VolumeController::class, 'index'])->name('volumes');
-Route::get('/media/{library}/{collection}/{volume}', [App\Http\Controllers\VolumeController::class, 'readVolume'])->name('volume');
-Route::get('/media/{library}/{collection}/{volume}/uncompress', [App\Http\Controllers\VolumeController::class, 'uncompressVolume'])->name('uncompress-volume');
-Route::get('/media/{library}/{collection}/{volume}/{page}', [App\Http\Controllers\VolumeController::class, 'readPage'])->name('read-page');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', [App\Http\Controllers\LibraryController::class, 'index']);
+    Route::get('/home', [App\Http\Controllers\LibraryController::class, 'index'])->name('home');
+    Route::get('/media/{library}/', [App\Http\Controllers\CollectionController::class, 'index'])->name('libraries');
+    Route::get('/media/{library}/{collection}/', [App\Http\Controllers\VolumeController::class, 'index'])->name('volumes');
+    Route::get('/media/{library}/{collection}/{volume}', [App\Http\Controllers\VolumeController::class, 'readVolume'])->name('volume');
+    Route::get('/media/{library}/{collection}/{volume}/uncompress', [App\Http\Controllers\VolumeController::class, 'uncompressVolume'])->name('uncompress-volume');
+    Route::get('/media/{library}/{collection}/{volume}/{page}', [App\Http\Controllers\VolumeController::class, 'readPage'])->name('read-page');
+});
+
+
+
 
 
 
