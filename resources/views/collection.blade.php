dfe2da5ea935ec70313dad937630f4cfa5c4c1ec
Blazkowicz <bj.blazkowicz@gmail.com>
2020-11-11 15:48:25 +0100

    Redesign collections page

--- resources/views/collection.blade.php
+++ resources/views/collection.blade.php
@@ -3,14 +3,14 @@
 <div class="container">
     <div class="row">
         @foreach ($collections as $collection)
-        <a class="col-md-4" href="/media/{{ $collection->library_slug }}/{{ $collection->slug }}">
+        <a class="col-md-4" href="/media/{{ $collection->library_slug }}/{{ $collection->slug }}" style="margin-bottom: 20px;">
             <div class="card">
                 <div class="card-header">
                     {{ $collection->name }}<span class="badge badge-danger"
                         style="float: right;">{{ $collection->volumeLeft() }}</span>
                 </div>
                 <div class="card-body">
-                    <img src="{{ $collection->picture }}" alt="" itemprop="image" class="col-md-12">
+                    <img src="{{ $collection->picture }}" alt="" itemprop="image" class="col-md-12" style="width: 200; height: 350px">
                 </div>
             </div>
         </a>
