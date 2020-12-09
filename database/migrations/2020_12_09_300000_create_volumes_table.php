f3b3e067bdc338f5f9d88a0a9ba1c2b88f599bd5
Blazkowicz <bonnet.nicolas66@gmail.com>
2020-12-09 21:14:37 +0100

    Adding new database migrations

---
+++ database/migrations/2020_12_09_300000_create_volumes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVolumesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('volumes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('picture');
+            $table->string('extension');
+            $table->integer('pages');
+            $table->integer('collection_id');
+            $table->integer('language_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('volumes');
+    }
+}
